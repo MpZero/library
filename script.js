dca9450a9be129ec1f180f96e3139a5521bfabdb
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-09-14 15:01:44 -0300

    add button in the table to let you delete the desired book from the table and array

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //get the button by id, when clicked run addBookToLibrary
-const submitBtn = document.getElementById("btn").addEventListener("click", addBookToLibrary);
+// const submitBtn = document.getElementById("btn").addEventListener("click", addBookToLibrary);
 
+const submitBtn = document.getElementById("btn").addEventListener("click", addBookToLibrary, (e) => {
+ reloadTable(e)
+});
+
+//get the table elements to append a generated table
 const table = document.getElementById("table")
 const tbody = document.getElementById("tbody")
+
 
 //empty array to store books 
 let myLibrary = [{
@@ -10,6 +16,15 @@ let myLibrary = [{
 },
 {
   title: "The Count of Monte Cristo", author: "Alexandre Dumas", year: "1846", numPages: "1276", status: "Read"
+},
+{
+  title: "2", author: "Alexandre Dumas", year: "1846", numPages: "1276", status: "Read"
+},
+{
+  title: "3", author: "Alexandre Dumas", year: "1846", numPages: "1276", status: "Read"
+},
+{
+  title: "4", author: "Alexandre Dumas", year: "1846", numPages: "1276", status: "Read"
 }
 
 ];
@@ -20,7 +35,7 @@ displayBook()
 function addBookToLibrary() {
 
   //empty the table before adding a new book
-  reloadTable()
+  // reloadTable()
 
 
   //get the book data from the form inputs into an object
@@ -29,11 +44,19 @@ function addBookToLibrary() {
     author: document.getElementById("author").value,
     year: document.getElementById("year").value,
     numPages: document.getElementById("numPages").value,
-    status: document.querySelector("input[name=readStatus]:checked").value
+    status: document.querySelector("input[name=readStatus]:checked").value,
+    data: ""
+    
   };
 
+  for (let i = 0; i < myLibrary.length; i++) {
+    myLibrary[i].data = [i] // 
+  }
+ 
   //push the object data into the array
   myLibrary.push(book)
+
+  // reloadTable()
 
   //run function to display the books on the table
   displayBook() 
@@ -43,6 +66,8 @@ function addBookToLibrary() {
 
 function displayBook() {
   for (let i = 0; i < myLibrary.length; i++) {
+    
+    
     let tr = document.createElement("tr")
     let td1 = document.createElement("td")
     let td2 = document.createElement("td")
@@ -57,7 +82,7 @@ function displayBook() {
     btn.innerText = "Delete"
     btn.classList = "btnDel"
     // btn.id = "del"
-    // btn.setAttribute("data-index", 0)
+    btn.setAttribute("data-index", [i])
 
     tr.id = "tr" + [i]
 
@@ -77,27 +102,31 @@ function displayBook() {
     td4.innerHTML = myLibrary[i].numPages
     td5.innerHTML = myLibrary[i].status
 
-    console.log(myLibrary[i].title)
+    // console.log(myLibrary[i].title)
+    // myLibrary[i].data = "data-index" + [i] // data-index0
+    myLibrary[i].data = [i] // 
+
+    // console.log(myLibrary[i].data)
   }
 }
 
-function reloadTable() {
+function reloadTable(el) {
   // let tr = document.getElementById("table")
-  for (let i = 0; i < myLibrary.length; i++) {
+  el.parentElement.parentElement.remove()
 
-    let a = document.getElementById("tr" + [i])
-    a.remove()
+  // for (let i = 0; i < myLibrary.length; i++) {
+  
+  //   // let tr = document.getElementById("tr" + [i])
+  //   // tr.remove()
+  //   el.parentElement.parentElement.remove()
 
-  }
+  // }
 }
-// reloadTable()
 
 
-
-
+///////////////////////////////////////////////////////////////////////////
 //make a form to add books appear after clicking the "Click Here..." button
 const toggleForm = document.getElementById('toggleForm');
-// const sideShadow = document.getElementById("sidebar-shadow")
 
 toggleForm.addEventListener('click', () => {
   const form = document.getElementById('form');
@@ -109,11 +138,112 @@ toggleForm.addEventListener('click', () => {
   }
 });
 
+///////////////////////////////////////////////////////////////////////////
+
+
+// Works
+let deleteBtn = document.querySelector("#tbody").addEventListener("click", (e) => {
+  deleteBook(e.target)
+  // console.log(e.target)
+  
+}) 
+
+// function deleteBook(el) {
+//   for (let i = myLibrary.length - 1; i >= 0; --i) {
+//     // console.log(el.dataset.index)
+//     // console.log(el.getAttribute())
+
+//     // console.log(myLibrary[i].dataset.index)
+//     console.log(myLibrary[i].data)
+
+//     if (parseInt(myLibrary[i].data )  === el.dataset.index) {
+//       console.log("hey")
+//         // myLibrary.splice(i,1);
+//     }
+// }
+// // reloadTable()
+// // displayBook()
+
+// }
+
+function deleteBook(el) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    // console.log(el.dataset.index) // 0
+    // console.log(myLibrary[i].data) // data-index0
+    // console.log(el.getAttribute("data-index")) // 0
+    
+    // console.log(myLibrary[i].data)
+
+
+    if (myLibrary[i].data  == el.dataset.index) {
+      // console.log("hey")
+        myLibrary.splice(i,1);
+        console.table(myLibrary)
+        reloadTable(el)
+      }
+    }
+}
+
+/////Deletes the DOM table////
+// function deleteBook(el) {
+//   let a = document.getElementById("tr0")
+//   if(el.classList.contains("btnDel")){
+//     el.parentElement.parentElement.remove();
+//     myLibrary.splice()
+//   }
+//  reloadTable()
+// }
+
+/////
+// for (let i = 0; i < myLibrary.length; i++) {
+//   // const element = myLibrary[i];
+//   let a = document.querySelectorAll('[data-index="{i}"]').addEventListener("click", (e) => {
+//     console.log(e.target)
+  
+// })
+// }
+
+// document.querySelector('[data-index="{i}"]').addEventListener("click", (e) => {
+//   console.log(e.target)
+// }) 
 
 
 
-//Delete books    
-// const deleteBtn = document.querySelectorAll(".yo");
+
+
+
+// const elements1 = document.querySelectorAll(myLibrary.0);
+// console.log(elements1); // 👉️ [div, div]
+
+
+// Delete books    
+// function a() {
+// for (let i = 0; i < myLibrary.length; i++) {
+//   const deleteBtn = document.querySelectorAll("data-index", [i]);
+  
+//   deleteBtn.forEach(btn => {
+//     btn.addEventListener("click", () => deleteBook())
+//   });
+// }
+
+// }
+
+// for (let i = 0; i < myLibrary.length; i++) {
+//   console.log("hey")
+//   // const deleteBtn = document.querySelectorAll(`[data-id=+${i}]`)
+//   const deleteBtn = document.querySelectorAll('[data-index=' + {i}+"]")
+//   deleteBtn.forEach(btn => {
+//     btn.addEventListener("click", () => console.log("hey"))
+//   });
+  
+// }
+// const deleteBtn = document.querySelectorAll("data-index"[i]);
+  
+//   deleteBtn.forEach(btn => {
+//     btn.addEventListener("click", () => deleteBook())
+//   });
+
+// const deleteBtn = document.querySelectorAll("data-index"[i]);
 
 // deleteBtn.forEach(btn => {
 //   btn.addEventListener("click", () => deleteBook())
@@ -121,14 +251,21 @@ toggleForm.addEventListener('click', () => {
 
 
 // function deleteBook() {
-//   this.remove();
-//   // myLibrary.pop()
-//   // allDeleteBtns.parentElement.remove()
-//   // let a = allDeleteBtns
-//   // a.remove()
-//   // delBook = ""
-//   console.log(myLibrary)
+//   console.log("hey")
+// //   for (var i = myArray.length - 1; i >= 0; --i) {
+// //     if (myArray[i].field == "money") {
+// //         myArray.splice(i,1);
+// //     }
 // }
+
+  // this.remove();
+  // myLibrary.pop()
+  // allDeleteBtns.parentElement.remove()
+  // let a = allDeleteBtns
+  // a.remove()
+  // delBook = ""
+  // console.log(myLibrary)
+
 
 ///////////////////////////////////////////////////
 
@@ -136,44 +273,3 @@ toggleForm.addEventListener('click', () => {
 //    document.querySelectorAll(`[data-id="${i}]`).remove()
 // );
 
-
-// function displayBook () {
-
-//   //create a new table row with cells to store the new book
-//   let table1 = document.createElement("tr")
-//   let td = document.createElement("td")
-//   let td = document.createElement("td")
-//   let td = document.createElement("td")
-//   let td = document.createElement("td")
-//   let td = document.createElement("td")
-//   let td = document.createElement("td")
-//   row6.classList = "btnTd"
-//   let btn = document.createElement("button")
-//   btn.innerText = "Delete"
-//   btn.classList = "btnDel"
-//   btn.id = "del"
-//   btn.setAttribute("data-index", 0)
-
-
-
-//   //append the new table to the DOM
-//   tbody.appendChild(table1)
-//   table1.appendChild(row1)
-//   table1.appendChild(row2)
-//   table1.appendChild(row3)
-//   table1.appendChild(row4)
-//   table1.appendChild(row5)
-//   table1.appendChild(row6)
-//   row6.appendChild(btn)
-
-
-//   //fill the table with the objects stored in the array
-//   myLibrary.map(x => row1.innerText = x["title"]);
-//   myLibrary.map(x => row2.innerText = x["author"]);
-//   myLibrary.map(x => row3.innerText = x["year"]);
-//   myLibrary.map(x => row4.innerText = x["numPages"]);
-//   myLibrary.map(x => row4.innerText = x["numPages"]);
-//   myLibrary.map(x => row5.innerText = x["status"]);
-
-
-// }
